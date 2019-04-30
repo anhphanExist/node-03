08ece1853c914073bde60fdb4e3f622d7f1e6adc
Anh Phan <anhphan.csf@gmail.com>
2019-04-30 21:48:28 +0700

    first time edit todoController

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,127 +1,97 @@
 const TodoObject = require("../models/TodoObject");
 
 async function get(request, response) {
-    const id = request.params.id;
-
-    const todo = await TodoObject.findOne({_id: id}).lean();
-    if (!todo) {
-        return response.send({
-            success: false,
-            message: "TodoObject not found."
-        });
-    }
-
-    response.send({
-        data: todo,
-        success: true,
-    });
-
-}
-
-async function list(request, response) {
-    const {page, limit} = request.query;
-    const p = page ? parseInt(page, 10) : 1;
-    const l = limit ? parseInt(limit, 10) : 10;
-    const skip = (p - 1) * l;
-
-    const todoList = await TodoObject.find({})
-        .skip(skip)
-        .limit(l)
-        .sort({
-            created: -1
-        })
-        .lean();
-
-    response.send(todoList);
-}
-
-async function create(request, response) {
-    const title = request.body.title;
-
-    if (!title) {
-        return response.send({
-            success: false,
-            message: "Title must be not empty."
-        });
-    }
-
     try {
-        const todo = new TodoObject({
-            title: title
-        });
-
-        const doc = await todo.save();
-
-        return response.send({
-            data: doc.toJSON(),
-            success: true
+        const id = request.params.id;
+        const todo = await TodoObject.findById(id).lean();
+        response.send({
+            data: todo,
+            success: true,
         });
     } catch (err) {
         response.send({
             success: false,
-            message: err.message,
+            message: `Get Object Failed! Details: ${err.message}`
+        });
+    }
+}
+
+async function list(request, response) {
+    try {
+        const todoList = await TodoObject.find().lean();
+        response.send(todoList);
+    } catch (err) {
+        response.send(`Load List Failed! Details: ${err.message}`);
+    }
+}
+
+async function create(request, response) {
+    const title = request.body.title;
+    if (!title) {
+        return response.send({
+            success: false,
+            message: "Title can't be empty."
+        });
+    }
+    try {
+        const todo = new TodoObject({
+            title: title
+        });
+        const doc = await todo.save();
+        return response.send({
+            success: true,
+            data: doc.toJSON(),
+        });
+    } catch (err) {
+        response.send({
+            success: false,
+            message: `Create Failed! Details: ${err.message}`
         });
     }
 }
 
 async function update(request, response) {
     const title = request.body.title;
-
     if (!title) {
         return response.send({
             success: false,
-            message: "Title must be not empty."
-        })
-    }
-
-    const id = request.params.id;
-    const todo = await TodoObject.findOne({_id: id}).lean();
-    if (!todo) {
-        return response.send({
-            success: false,
-            message: "TodoObject not found."
+            message: "Title can't be empty."
         });
     }
-
     try {
-        await TodoObject.updateOne(
-            {_id: id},
-            {
-                $set: {
-                    title
-                }
-            }
+        const id = request.params.id;
+        const todo = await TodoObject.findByIdAndUpdate(
+            {id},
+            {$set: {title}},
+            {new: true},
         );
-
         response.send({
             success: true,
-            data: true,
+            data: todo
         });
-    } catch (e) {
+    } catch (err) {
         response.send({
             success: false,
-            message: e.message,
+            message: `Update Failed! Details: ${err.message}`
         });
     }
+
 }
 
 async function del(request, response) {
     const id = request.params.id;
-
-    const todo = await TodoObject.findOne({_id: id}).lean();
-    if (!todo) {
-        return response.send({
+    try {
+        await TodoObject.findByIdAndDelete(id);
+        response.send({
+            success: true,
+            data: true
+        });
+    } catch (err) {
+        response.send({
             success: false,
-            message: "Todo not found."
+            message: `Delete Failed! Details: ${err.message}`
         });
     }
-
-    await TodoObject.deleteOne({_id: id});
-
-    response.send({
-        success: true,
-        data: true,
-    });
 }
 
 module.exports.get = get;
