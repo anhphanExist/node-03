0c4929966b458ad4a1fb79a2b2f37cd856fb122d
Anh Phan <anhphan.csf@gmail.com>
2019-04-30 23:06:24 +0700

    what an embarrased mistake

--- src/router.js
+++ src/router.js
@@ -8,9 +8,9 @@ router.get("/", function (request, response) {
 router.get("/todos", todoController.list);
 router.get("/todos/:id", todoController.get);
 router.post("/todos", todoController.create);
-router.post("todos/:id", todoController.updateTitleOnly);
-router.post("todos/:id/toggle", todoController.updateStatusOnly);
-router.delete("todos/:id", todoController.del);
-router.delete("todos", todoController.delAll);
+router.post("/todos/:id", todoController.updateTitleOnly);
+router.post("/todos/:id/toggle", todoController.updateStatusOnly);
+router.delete("/todos/:id", todoController.del);
+router.delete("/todos", todoController.delAll);
 
 module.exports = router;
